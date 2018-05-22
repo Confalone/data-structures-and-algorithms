34a3ea9048f68ff3f5de601d745121f193c8548b
Tyler Confalone <tconfalone@gmail.com>
2018-05-21 23:53:30 -0700

    whiteBoard2 done

--- arrays/__test__/array-shift.spec.js
+++ arrays/__test__/array-shift.spec.js
@@ -7,9 +7,9 @@ describe('insertShiftArray', () => {
     expect(arrayShift.insertShiftArray([1, 2, 4, 5], 3)).toEqual([1, 2, 3, 4, 5]);
   });
   it(' should take an array and input a number in the middle of it', () => {
-    expect(arrayShift.insertShiftArray([1, 2, 3, 4, 6, 7, 8, 9], 5)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    expect(arrayShift.insertShiftArray([1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12], 7)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
   });
   it(' should take an array and input a number in the middle of it', () => {
-    expect(arrayShift.insertShiftArray([4,8,15,23,42], 16)).toEqual([4,8,15,16,23,42]);
+    expect(arrayShift.insertShiftArray([4,8,23,42], 16)).toEqual([4,8,16,23,42]);
   });
 });
