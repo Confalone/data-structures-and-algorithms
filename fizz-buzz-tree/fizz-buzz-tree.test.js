193bf35a2f4a8955eac585f99ab151cd5b76d90d
Tyler Confalone <tconfalone@gmail.com>
2018-06-28 16:48:05 -0700

    FizzBuzzTesting

--- fizz-buzz-tree/fizz-buzz-tree.test.js
+++ fizz-buzz-tree/fizz-buzz-tree.test.js
@@ -2,6 +2,23 @@
 
 describe('fizzbuzztree', () => {
 
+  // it('should return a value of 3 at i', () => {
+    
+  //   let i = 0; 
+
+  //   for (i = 1; i <= 100; i++) {
+  //     var output = '';
+
+  //     if (i % 3 === 0) {output += 'Fizz';}
+  //     if (i % 5 === 0) {output += 'Buzz';}
+
+  //     if (output == '') {output = i;}
+
+  //     console.log(output);
+  //   }
+  //   expect(i[3]).toBe(Fizz);
+  // });
+
   it('should return a value of 3 at i', () => {
     
     let i = 0; 
@@ -16,6 +33,6 @@ describe('fizzbuzztree', () => {
 
       console.log(output);
     }
-    expect(i[3]).toBe(Fizz);
+    expect(output[3]).toBe(Fizz);
   });
 });
