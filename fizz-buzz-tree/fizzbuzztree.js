bface32f9c1e6cb99a1b6501f8f63e9fe7714dc0
Tyler Confalone <tconfalone@gmail.com>
2018-06-28 07:31:45 -0700

    FizzBuzzTesting

--- fizz-buzz-tree/fizzbuzztree.js
+++ fizz-buzz-tree/fizzbuzztree.js
@@ -1,12 +1,14 @@
 'use strict';
 
+let i = 0; 
+
 for (i = 1; i <= 100; i++) {
   var output = '';
 
   if (i % 3 === 0) {output += 'Fizz';}
   if (i % 5 === 0) {output += 'Buzz';}
 
-  if output == '') {output = i;}
+  if (output == '') {output = i;}
 
   console.log(output);
 }
