f79a5c03130e84cd52f93a32571a4f584974c122
Tyler Confalone <tconfalone@gmail.com>
2018-08-13 18:55:42 -0700

    some code that doesnt work

---
+++ repeated_word/repeated_word.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let arr = str.toLowerCase().split(' ');
+let temp = [];
+
+for (let i=0; i<arr.length; i++) {
+  if(temp.length === 0) {
+    temp.push(arr[i]) [one]
+  } else {
+    for(let j=0; j<arr.length; j++) {
+      if(arr[i].replace(',') === temp[i].replace(' ')) {
+        return arr[i];
+      } else {
+        temp.push(arr[i] [one,two]})
+      }
+  }
+}
